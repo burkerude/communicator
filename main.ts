97d7422e5811960da0447a7bec24a6e02ed66429
burkerude <49853479+burkerude@users.noreply.github.com>
2020-10-09 23:06:33 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,18 +23,17 @@ function settings () {
     } else if (menu_selection == 1) {
     	
     } else if (menu_selection == 2) {
-        basic.showString(username)
+        basic.showString("" + (username))
         while (!(input.buttonIsPressed(Button.A))) {
             if (input.buttonIsPressed(Button.B)) {
                 settings()
             }
         }
-        _type()
-        while (finishtype == 0) {
+        text_input()
+        while (0 == 0) {
         	
         }
-        username = current_stringtype
-        finishtype = 0
+        username = 0
         settings()
     } else if (menu_selection == 3) {
         messages()
@@ -43,6 +42,25 @@ function settings () {
 function Drafts () {
 	
 }
+input.onButtonPressed(Button.AB, function () {
+    basic.pause(100)
+    if (input.buttonIsPressed(Button.AB)) {
+        for (let index = 0; index <= 3; index++) {
+            basic.showNumber(index)
+            if (!(input.buttonIsPressed(Button.AB))) {
+                break;
+            }
+            basic.pause(100)
+            if (index == 3) {
+                _break = 1
+                basic.pause(10)
+                Home()
+            }
+        }
+    } else {
+        AB = 1
+    }
+})
 function messages () {
     current_menu = ["MESSAGES", "DRAFTS", "SETTINGS"]
     Menu()
@@ -55,26 +73,6 @@ function messages () {
         Drafts()
     } else if (menu_selection == 2) {
         settings()
-    }
-}
-function _type () {
-    current_stringtype = ""
-    for (let value of alphabet) {
-        basic.showString(value)
-        while (!(input.buttonIsPressed(Button.B))) {
-            if (input.buttonIsPressed(Button.A)) {
-                if (value == "^") {
-                    break;
-                }
-                current_stringtype = "" + current_stringtype + value
-                _type()
-            }
-            if (input.buttonIsPressed(Button.AB)) {
-                basic.pause(350)
-                basic.showString(current_stringtype)
-                finishtype = 1
-            }
-        }
     }
 }
 function Menu () {
@@ -99,34 +97,29 @@ function Menu () {
         }
     }
 }
-let _break = 0
+function text_input () {
+    Text_input = 1
+    alphabet2 = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ^"
+    visable_charactor = 0
+    while (AB == 0) {
+        if (input.buttonIsPressed(Button.A)) {
+            visable_charactor += 1
+        } else if (input.buttonIsPressed(Button.B)) {
+            visable_charactor += -1
+        }
+        basic.showString("this".charAt(0))
+    }
+}
+let visable_charactor = 0
+let alphabet2 = ""
+let Text_input = 0
 let menu_place = 0
-let current_stringtype = ""
-let finishtype = 0
+let AB = 0
+let _break = 0
 let menu_selection = 0
 let current_menu: string[] = []
-let alphabet: string[] = []
 let username = ""
 username = "burkerude"
 let page = 1
-alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_", "^"]
+let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_", "^"]
 Home()
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.AB)) {
-        basic.pause(100)
-        if (input.buttonIsPressed(Button.AB)) {
-            for (let index = 0; index <= 3; index++) {
-                basic.showNumber(index)
-                if (!(input.buttonIsPressed(Button.AB))) {
-                    break;
-                }
-                basic.pause(100)
-                if (index == 3) {
-                    _break = 1
-                    basic.pause(10)
-                    Home()
-                }
-            }
-        }
-    }
-})
