3e9ae38a2833fe95786c0a9af778709ecfafe46c
burkerude <49853479+burkerude@users.noreply.github.com>
2020-10-11 14:48:08 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,169 +2,55 @@ enum RadioMessage {
     message1 = 49434,
     Ping = 61148
 }
-function check () {
-    load_speed = 25
-    led.plot(0, 0)
-    basic.pause(load_speed)
-    led.plot(1, 0)
-    basic.pause(load_speed)
-    led.plot(2, 0)
-    basic.pause(load_speed)
-    led.plot(3, 0)
-    basic.pause(load_speed)
-    led.plot(4, 0)
-    basic.pause(load_speed)
-    led.plot(4, 1)
-    basic.pause(load_speed)
-    led.plot(4, 2)
-    basic.pause(load_speed)
-    led.plot(4, 3)
-    basic.pause(load_speed)
-    led.plot(4, 4)
-    basic.pause(load_speed)
-    led.plot(3, 4)
-    basic.pause(load_speed)
-    led.plot(2, 4)
-    basic.pause(load_speed)
-    led.plot(1, 4)
-    basic.pause(load_speed)
-    led.plot(0, 4)
-    basic.pause(load_speed)
-    led.plot(0, 3)
-    basic.pause(load_speed)
-    led.plot(0, 2)
-    basic.pause(load_speed)
-    led.plot(0, 1)
-    basic.pause(200)
-    basic.clearScreen()
-}
 function button_reset () {
-    a = 0
-    b = 0
-    AB = 0
+    a = false
+    b = false
+    AB = false
+}
+function loading2 () {
+    loading = true
+    while (loading == true) {
+        for (let index = 0; index <= 4; index++) {
+            led.toggle(index, 0)
+            basic.pause(10)
+        }
+        led.plot(0, 0)
+        for (let index = 0; index <= 3; index++) {
+            led.toggle(4, 1 + index)
+            basic.pause(10)
+        }
+        for (let index = 0; index <= 3; index++) {
+            led.toggle(3 - index, 4)
+            basic.pause(10)
+        }
+        for (let index = 0; index <= 2; index++) {
+            led.toggle(0, 3 - index)
+            basic.pause(10)
+        }
+    }
 }
 input.onButtonPressed(Button.A, function () {
-    a = 1
+    a = true
 })
-function Home () {
-    current_menu = ["MESSAGES", "SETTINGS"]
-    Menu()
-    while (menu_selection == 100) {
-        basic.pause(25)
-    }
-    if (menu_selection == 0) {
-        messages()
-    } else if (menu_selection == 1) {
-        settings()
-    } else if (menu_selection == 2) {
-    	
-    }
-}
-function settings () {
-    current_menu = ["null", "null2", "USERNAME", "^"]
-    Menu()
-    while (menu_selection == 100) {
-        basic.pause(25)
-    }
-    if (menu_selection == 0) {
-    	
-    } else if (menu_selection == 1) {
-    	
-    } else if (menu_selection == 2) {
-        basic.showString("" + (username))
-        while (true) {
-            if (a == 1) {
-                working_text = ""
-                text_input()
-                while (text_entered == 0) {
-                	
-                }
-                text_entered = 0
-                username = working_text
-                settings()
-                break;
-            } else if (b == 1) {
-                settings()
-                break;
-            }
-        }
-    } else if (menu_selection == 3) {
-        Home()
-    }
-}
-function Drafts () {
-    let drafts_content: string[] = []
-    current_draft = 0
-    current_menu = ["0", "1", "2", "3", "4"]
-    Menu()
-    while (menu_selection == 100) {
-        basic.pause(25)
-    }
-    current_draft = menu_selection
-    current_menu = ["READ", "WRITE", "TO:", "SEND", "^"]
-    Menu()
-    while (menu_selection == 100) {
-        basic.pause(25)
-    }
-    if (menu_selection == 0) {
-        basic.showString("" + (drafts_content[current_draft]))
-        Drafts()
-    } else if (menu_selection == 1) {
-        working_text = ""
-        text_input()
-        while (text_entered == 0) {
-        	
-        }
-        text_entered = 0
-        drafts_content[current_draft] = working_text
-        check()
-        basic.showString("" + (drafts_content[current_draft]))
-        Drafts()
-    } else if (menu_selection == 2) {
-        let drafts_recipients: string[] = []
-        working_text = ""
-        text_input()
-        while (text_entered == 0) {
-        	
-        }
-        text_entered = 0
-        drafts_recipients[current_draft] = working_text
-    } else if (menu_selection == 3) {
-    	
-    } else if (menu_selection == 4) {
-    	
-    }
-}
 input.onButtonPressed(Button.AB, function () {
-    AB = 1
+    AB = true
 })
-function messages () {
-    current_menu = ["INBOX", "DRAFTS"]
-    Menu()
-    while (menu_selection == 100) {
-        basic.pause(25)
-    }
-    if (menu_selection == 0) {
-        Drafts()
-    } else if (menu_selection == 1) {
-        Drafts()
-    }
-}
 input.onButtonPressed(Button.B, function () {
-    b = 1
+    b = true
 })
 function Menu () {
     let menu = 0
     menu_selection = 100
     menu_place = 0
     while (menu == 0) {
-        if (a == 1) {
+        let current_menu: number[] = []
+        if (a == true) {
             menu_place += -1
             button_reset()
-        } else if (b == 1) {
+        } else if (b == true) {
             menu_place += 1
             button_reset()
-        } else if (AB == 1) {
+        } else if (AB == true) {
             menu_selection = menu_place
             button_reset()
             break;
@@ -172,17 +58,23 @@ function Menu () {
         basic.showString("" + (current_menu[menu_place]))
     }
 }
+/**
+ * put whatever characters you want in the alphabet
+ */
+/**
+ * any other way of capturing button events would miss button presses
+ */
 function text_input () {
-    text_entered = 0
-    Text_input = 1
     alphabet = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+    text_entered = false
+    Text_input = 1
     visable_charactor = 0
-    while (text_entered == 0) {
-        while (AB == 0) {
-            if (b == 1) {
+    while (text_entered == false) {
+        while (AB == true) {
+            if (b == true) {
                 visable_charactor += 1
                 button_reset()
-            } else if (a == 1) {
+            } else if (a == true) {
                 visable_charactor += -1
                 button_reset()
             }
@@ -190,7 +82,7 @@ function text_input () {
         }
         button_reset()
         if (alphabet.charAt(visable_charactor) == "#") {
-            text_entered = 1
+            text_entered = true
         } else {
             working_text = "" + working_text + alphabet.charAt(visable_charactor)
             basic.clearScreen()
@@ -198,18 +90,23 @@ function text_input () {
         }
     }
 }
+/**
+ * This is how to use the function
+ */
 let visable_charactor = 0
-let alphabet = ""
 let Text_input = 0
+let alphabet = ""
 let menu_place = 0
-let current_draft = 0
-let text_entered = 0
-let working_text = ""
 let menu_selection = 0
-let current_menu: string[] = []
-let AB = 0
-let b = 0
-let a = 0
-let load_speed = 0
-let username = ""
-username = "burkerude"
+let loading = false
+let AB = false
+let b = false
+let a = false
+let working_text = ""
+let text_entered = false
+text_input()
+while (text_entered == false) {
+	
+}
+text_entered = false
+let Text_Output = working_text
